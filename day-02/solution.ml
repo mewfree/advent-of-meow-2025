2e4ddbcd284da738cb2e8e490fb11107610142da
Damien Gonot <damien.gonot@gmail.com>
2025-12-09 21:24:06 -0500

    day 2 part 2

--- day-02/solution.ml
+++ day-02/solution.ml
@@ -27,4 +27,35 @@ let all_invalid_ids =
 
 let sum = List.fold_left (+) 0 all_invalid_ids
 
-let () = print_endline ("Result: " ^ string_of_int sum)
+let () = print_endline ("Part 1: " ^ string_of_int sum)
+
+let is_invalid_id_p2 n =
+  let s = string_of_int n in
+  let len = String.length s in
+  len > 1 &&
+  let rec check_pattern pattern_len =
+    if pattern_len > len / 2 then false
+    else if len mod pattern_len = 0 then
+      let pattern = String.sub s 0 pattern_len in
+      let rec all_match pos =
+        pos >= len ||
+        (String.sub s pos pattern_len = pattern && all_match (pos + pattern_len))
+      in
+      all_match pattern_len || check_pattern (pattern_len + 1)
+    else check_pattern (pattern_len + 1)
+  in
+  check_pattern 1
+
+let find_invalid_ids_in_range_p2 (start_id, end_id) =
+  let rec aux acc current =
+    if current > end_id then List.rev acc
+    else aux (if is_invalid_id_p2 current then current :: acc else acc) (current + 1)
+  in
+  aux [] start_id
+
+let all_invalid_ids_p2 =
+    List.concat_map find_invalid_ids_in_range_p2 ranges
+
+let sum_p2 = List.fold_left (+) 0 all_invalid_ids_p2
+
+let () = print_endline ("Part 2: " ^ string_of_int sum_p2)
